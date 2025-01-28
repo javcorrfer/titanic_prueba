37df734c6937d8fd6788a99342c0d2877f9ed7a3
javcorrfer <javiercorralesfernandez@gmail.com>
2025-01-28 21:32:11 +0100

    actualizacion webapp

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 import streamlit as st
+import pickle
 
 def main():
 
-    st.write("Hola Mundo!")
+    # Cargamos el modelo entrenado
+    with open("modelo_entrenado.pkl", "rb") as file:
+        modelo = pickle.load(file)
+
+    st.title("Prueba Modelo ML")
+
+    st.text("En qué ciudad ha embarcado?")
+    ciudad = st.text_input(label="Lugar de embarque", value=None, key="str")
+
+    st.write(f"Ciudad en la que ha embarcado: {ciudad}")
+
+
 
 if __name__ == "__main__":
     main()
